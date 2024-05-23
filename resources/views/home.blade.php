02dbbe808e3882dd64c4b9f9dd157ee0038a712d
simone-danzeri <s.danzeri@icloud.com>
2024-05-23 16:15:10 +0200

    feat: add header with nav bar

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,4 +1,4 @@
 @extends('layouts.app')
 @section('content')
-    <h1>ciao sono la homepage</h1>
+    <h1 class="text-center my-3">Welcome to your Comics List</h1>
 @endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -16,7 +16,22 @@
 </head>
 
 <body>
-
+    <header>
+        <nav class="navbar navbar-expand-lg bg-body-secondary">
+            <div class="container-fluid">
+              <a class="navbar-brand" href="{{ route('home') }}">HOMEPAGE</a>
+              <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+              </button>
+              <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                <div class="navbar-nav">
+                  <a class="nav-link" href="{{ route('comics.index') }}">Comics List</a>
+                  <a class="nav-link" href="{{ route('comics.create') }}">Add a new comic</a>
+                </div>
+              </div>
+            </div>
+        </nav>
+    </header>
     <main>
         @yield('content')
     </main>
