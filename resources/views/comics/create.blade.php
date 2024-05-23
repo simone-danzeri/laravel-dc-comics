9fc336490fb4981f7c8def94c721326709857039
simone-danzeri <s.danzeri@icloud.com>
2024-05-23 15:44:42 +0200

    feat: CRUD Create

---
+++ resources/views/comics/create.blade.php
@@ -0,0 +1,38 @@
+@extends('layouts.app')
+@section('content')
+    <h1 class="text-center my-3">Add a new comic to your collection</h1>
+    <div class="container py-5">
+        <form action="{{ route('comics.store') }}" method="post">
+            @csrf
+            <div class="mb-3">
+                <label for="title" class="form-label">TITLE</label>
+                <input type="text" class="form-control" id="title" name="title">
+            </div>
+            <div class="mb-3">
+                <label for="thumb" class="form-label">THUMB</label>
+                <input type="text" class="form-control" id="thumb" name="thumb">
+            </div>
+            <div class="mb-3">
+                <label for="price" class="form-label">PRICE</label>
+                <input type="text" class="form-control" id="price" name="price">
+            </div>
+            <div class="mb-3">
+                <label for="series" class="form-label">SERIES</label>
+                <input type="text" class="form-control" id="series" name="series">
+            </div>
+            <div class="mb-3">
+                <label for="sale_date" class="form-label">SALE DATE</label>
+                <input type="text" class="form-control" id="sale_date" name="sale_date">
+            </div>
+            <div class="mb-3">
+                <label for="type" class="form-label">COMIC TYPE</label>
+                <input type="text" class="form-control" id="type" name="type">
+            </div>
+            <div class="mb-3">
+                <label for="description" class="form-label">DESCRIPTION</label>
+                <textarea name="description" id="description" rows="5" cols="50"></textarea>
+            </div>
+            <button type="submit" class="btn btn-primary">Submit</button>
+        </form>
+    </div>
+@endsection
