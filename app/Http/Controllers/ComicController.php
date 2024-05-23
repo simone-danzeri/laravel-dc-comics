49f6bc06b72c41085020c5ff3dc14f5f5e2ac051
simone-danzeri <s.danzeri@icloud.com>
2024-05-23 15:51:06 +0200

    feat: CRUD Store

--- app/Http/Controllers/ComicController.php
+++ app/Http/Controllers/ComicController.php
@@ -39,7 +39,17 @@ class ComicController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $data = $request->all();
+        $newComic = new Comic();
+        $newComic->title = $data['title'];
+        $newComic->thumb = $data['thumb'];
+        $newComic->price = $data['price'];
+        $newComic->series = $data['series'];
+        $newComic->sale_date = $data['sale_date'];
+        $newComic->type = $data['type'];
+        $newComic->description = $data['description'];
+        $newComic->save();
+        return redirect()->route('comics.show', $newComic->id);
     }
 
     /**
