8485a949cfd5b31366b9ce5acd830635d023dbc2
simone-danzeri <s.danzeri@icloud.com>
2024-05-27 16:20:15 +0200

    feat: modal for confirmation on delete (human code and thought process)

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,3 +4,18 @@ import * as bootstrap from 'bootstrap';
 import.meta.glob([
     '../img/**'
 ])
+
+// Modal to confirm deletion of on element
+
+// Step 1.
+// Remove default behaviour from Delete button we created with CRUD destroy
+
+// Step 2a.
+// On click on 'Delete', we open the modal
+
+// Step 2b.
+// Populate the modal with his correspondig element's body
+
+// Step 3.
+// Send the original form (CRUD destroy button)
+
--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -37,4 +37,23 @@
             @endforeach
         </div>
     </div>
+
+    {{-- Modale --}}
+<div class="modal fade" id="confirmModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h1 class="modal-title fs-5" id="confirmModalLabel">Do you really want to delete this comic?</h1>
+          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+        </div>
+        <div class="modal-body">
+            {{-- qui ci andrà il testo della modale --}}
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+          <button type="button" class="btn btn-danger" id="modal-confirm-delete">Delete</button>
+        </div>
+      </div>
+    </div>
+</div>
 @endsection
