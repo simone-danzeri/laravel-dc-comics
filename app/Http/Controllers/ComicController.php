afac1791e9169ef99e2b975ba2b91180ec7f3490
simone-danzeri <s.danzeri@icloud.com>
2024-05-28 13:44:56 +0200

    feat: created custom function to validate formData

--- app/Http/Controllers/ComicController.php
+++ app/Http/Controllers/ComicController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Comic;
+use Illuminate\Support\Facades\Validator;
 
 class ComicController extends Controller
 {
@@ -39,28 +40,9 @@ class ComicController extends Controller
      */
     public function store(Request $request)
     {
-        $validate = $request->validate(
-            [
-                'title' => 'required|max:99',
-                'thumb' => 'required',
-                'price' => 'required',
-                'series' => 'required|max:99',
-                'sale_date' => 'required',
-                'type' => 'required|max:49',
-                'description' => 'nullable|min:10|max:5000',
-            ],
-            [
-                'title.required' => 'Please insert a title for this new comic',
-                'thumb.required' => "Plese insert the comic's image URL",
-                'price.required' => "Plese insert the comic's price",
-                'series.required' => "Plese insert this comic's serie",
-                'sale_date.required' => "Plese insert when they released this comic",
-                'type.required' => "Plese insert this comic's type",
-                'description.min' => 'The description must be at least 10 characters long',
-                'description.max' => "The description can't be more than 5000 characters long"
-            ]
-        );
         $formData = $request->all();
+        // Richiamo la funzione per validare i dati forniti dall'utente
+        $this->validation($formData);
         $newComic = new Comic();
         // $newComic->title = $data['title'];
         $newComic->thumb = 'https://picsum.photos/200';
@@ -114,7 +96,34 @@ class ComicController extends Controller
      */
     public function update(Request $request, $id)
     {
-        $validate = $request->validate(
+        $formData = $request->all();
+        // Richiamo la funzione per validare i dati forniti dall'utente
+        $this->validation($formData);
+        $comic = Comic::findOrFail($id);
+        // La funzione update() mi fa il Mass Assignment dei dati presi dal forum e me li mette come nuovi valori degli attibuti del comic selezoinato tramite $id
+        $comic->update($formData);
+        // dd($formData, $comic);
+        return redirect()->route('comics.show', ['comic' => $id]);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $comic = Comic::findOrFail($id);
+        $comic->delete();
+        return redirect()->route('comics.index');
+    }
+
+    // Function to validate data from user
+    // $formData -> the actual data that need to be validate
+    protected function validation($formData) {
+        $validator = Validator::make(
+            $formData,
             [
                 'title' => 'required|max:99',
                 'thumb' => 'required',
@@ -134,25 +143,7 @@ class ComicController extends Controller
                 'description.min' => 'The description must be at least 10 characters long',
                 'description.max' => "The description can't be more than 5000 characters long"
             ]
-        );
-        $formData = $request->all();
-        $comic = Comic::findOrFail($id);
-        // La funzione update() mi fa il Mass Assignment dei dati presi dal forum e me li mette come nuovi valori degli attibuti del comic selezoinato tramite $id
-        $comic->update($formData);
-        // dd($formData, $comic);
-        return redirect()->route('comics.show', ['comic' => $id]);
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        $comic = Comic::findOrFail($id);
-        $comic->delete();
-        return redirect()->route('comics.index');
+        )->validate();
+        return $validator;
     }
 }
